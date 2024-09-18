3eaf183b9f15d2783c58a441429cdf0b1a8d3771
jamallasomasekhar <somasekharjamalla@gmail.com>
2024-09-18 08:44:00 +0530

    first commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, Azure Web Apps!"
+    return "Hello, Azure Web Apps! , i am somasekhar"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
